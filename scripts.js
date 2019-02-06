a501bcd914e0088f15b2ed8ff3c15faddbcd5785
stachedata <stachedata@gmail.com>
2019-02-06 16:11:51 -0500

    refactored

--- scripts.js
+++ scripts.js
@@ -1,30 +1,26 @@
 
-const requestArt = () => {
-    const header = new Headers({
-        'Access-Control-Allow-Origin': 'https://openaccess-api.clevelandart.org',
-    });
+const url = 'https://cors-anywhere.herokuapp.com/https://openaccess-api.clevelandart.org/api/artworks/?q=china&has_image=1&limit=1&indent=1'
 
-    fetch('https://cors-anywhere.herokuapp.com/https://openaccess-api.clevelandart.org/api/artworks/?q=china&has_image=1&limit=1&indent=1'
-    , {mode: 'cors', headers: header})
-        .then(response => response.json())
-        .then(response => response.data)
-        .then(response => response.map(piece => ({
-            img: piece.images.web.url,
-            desc: piece.wall_description
-        }))).then(resource => resource.map(render))
-        .catch(function(error) {
-            console.log('fetch failed: ', error.message)
-        })
-        
-        const art = document.getElementById('art')
+const requestArt = (url) => {
+    fetch( url , {mode: 'cors', headers: {'Access-Control-Allow-Origin': 'https://openaccess-api.clevelandart.org'}})
+    .then(response => response.json())
+    .then(json => json.data)
+    .then(data => data.map(data => ({
+        image: data.images.web.url,
+        description: data.wall_description
+    }))).then(output => output.map(render))
+    .catch(function(error) {
+        console.log('fetch failed: ', error.message)
+    })
+    
+    const artDiv = document.getElementById('art')
 
-        const render = piece => {
-            const desc = piece.desc
-            const node = document.createElement('div')
-            node.innerHTML = '<img src="' + piece.img + '"/>' + '<p>' + desc + '</p>'
-            art.appendChild(node)
-          return piece
-        }
+    const render = art => {
+        const node = document.createElement('div')
+        node.innerHTML = '<img src="' + art.image + '"/>' + '<p>' + art.description + '</p>'
+        artDiv.appendChild(node)
+        return artDiv
     }
+}
 
-requestArt();
+requestArt(url);
