9ef08b61bd140af0fd7340bd0dc7d51475bc7864
Chris Anastasi <stachedata@gmail.com>
2019-02-07 19:43:40 -0500

    displaying dogs only. refactored.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,25 @@
 
-const url = 'https://cors-anywhere.herokuapp.com/https://openaccess-api.clevelandart.org/api/artworks/?q=china&has_image=1&limit=1&indent=1'
+const url = 'https://cors-anywhere.herokuapp.com/https://openaccess-api.clevelandart.org/api/artworks/?q=dog&has_image=1&limit=300'
 
 const requestArt = (url) => {
-    fetch( url , {mode: 'cors', headers: {'Access-Control-Allow-Origin': 'https://openaccess-api.clevelandart.org'}})
+    return fetch( url , {mode: 'cors', headers: {'Access-Control-Allow-Origin': 'https://openaccess-api.clevelandart.org'}})
     .then(response => response.json())
-    .then(json => json.data)
-    .then(data => data.map(data => ({
-        image: data.images.web.url,
-        description: data.wall_description
-    }))).then(output => output.map(render))
     .catch(function(error) {
         console.log('fetch failed: ', error.message)
     })
-    
-    const artDiv = document.getElementById('art')
-
-    const render = art => {
-        const node = document.createElement('div')
-        node.innerHTML = '<img src="' + art.image + '"/>' + '<p>' + art.description + '</p>'
-        artDiv.appendChild(node)
-        return artDiv
-    }
 }
 
-requestArt(url);
+const loadJson = (url) =>{
+    requestArt(url)
+    .then(json => json.data.map(data => displayImage(data.images.web.url)))
+}
+
+const displayImage = (data) => {
+    const artDiv = document.getElementById('art')
+    const node = document.createElement('div')
+    node.innerHTML = '<img id=artImg src="' + data + '"/>'
+    artDiv.appendChild(node)
+    return artDiv
+}
+
+loadJson(url)
